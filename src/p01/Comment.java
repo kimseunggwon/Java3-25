a1dd9d9d6c34fa37716dd197a4c3097f0f45f595
kimseunggwon <dmltcl1@naver.com>
2021-03-25 11:33:01 +0900

    ㅋ

--- src/p01/Comment.java
+++ src/p01/Comment.java
@@ -4,7 +4,9 @@ public class Comment {
 
 	public static void main(String[] args) {
 		System.out.println("주석");
-     System.out.println("한 줄 주석");
+        //System.out.println("한 줄 주석");
 	}
-
+       /* 여러줄 주석 
+        * 
+        */
 }
---
+++ src/p01/textbook/Hello.java
@@ -0,0 +1,15 @@
+package p01.textbook;
+
+/*
+  작성자 :김승권
+  작성일 :2021/3/25
+ */
+
+public class Hello {
+  //프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello,welcome to the java world!");
+ 	} //end of main
+ 
+} // end of class
