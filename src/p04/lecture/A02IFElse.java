5d7af67f3be97caebcf43e6e3c9d27d9c5a620d4
kimseunggwon <dmltcl1@naver.com>
2021-03-31 08:09:35 +0900

    3/31

---
+++ src/p04/lecture/A02IFElse.java
@@ -0,0 +1,12 @@
+package p04.lecture;
+
+public class A02IFElse {
+
+	public static void main(String[] args) {
+		//else : if 키워드 다음에 작성되고 
+		//       if 조건의 false 일 때만 실행
+		
+
+	}
+
+}
---
+++ src/p04/textbook/IfExample.java
@@ -0,0 +1,21 @@
+package p04.textbook;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+	
+		int score =93;
+		
+		if(score >=90) {
+			System.out.println("점수가 90보다 큽니다");
+			System.out.println("동급은 A 입니다");
+		}
+		ㄴ
+		if(score <90) {
+			System.out.println("점수가 90점보다 작습니다");
+			System.out.println("동급은 B 입니다");
+		}
+
+	}
+
+}
